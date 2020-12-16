9ea8d2d2b34a1442955a41ffd7d3c5e994bda905
DESKTOP-UNBSCK8\dexte <Dextervanrenswoude@gmail.com>
2020-12-16 17:55:15 +0100

    prototype2.0unitygame

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefab;
+    private float spawnRangeX = 20;
+
+    private float spawnPosZ = 20;
+
+    private float startDelay = 2.0f;
+
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {} 
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefab.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-20, 20), 0, spawnPosZ);
+        Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
+    }
+    
+}
